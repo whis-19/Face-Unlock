0cdaded115ab0b4bfef6c5b4e3b13863f8830df7
Muhammad Abdullah <ma3472995@gmail.com>
2024-09-28 13:13:54 +0500

    face rocgnitin exception issue

--- Imports.py
+++ Imports.py
@@ -4,15 +4,8 @@ import pyautogui
 import threading
 import time
 import keyboard
+import face_recognition
 
 
-
-
-
-# Control loop variable
-
-# Initialize video capture and face mesh
-cam = cv2.VideoCapture(0)
-faceMesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
-screenWidth, screenHeight = pyautogui.size()
-print("Screen Width:", screenWidth, "Screen Height:", screenHeight)  # Debugging screen size
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(refine_landmarks=True)
